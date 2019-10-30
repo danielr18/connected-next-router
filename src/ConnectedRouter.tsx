3a4ded8f10ed3adb87db32984d93d583898e0965
Daniel Reinoso <danielreinoso1807@gmail.com>
2019-10-30 18:21:04 -0300

    Update react-redux to 7.1.0 (with hooks)

--- src/ConnectedRouter.tsx
+++ src/ConnectedRouter.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useRef } from 'react'
-import { ReactReduxContext, ReactReduxContextValue } from 'react-redux'
+import React, { useEffect, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import NextRouter, { SingletonRouter } from 'next/router'
 import { onLocationChanged } from './actions'
 import patchRouter from './patchRouter'
@@ -10,7 +10,6 @@ type ConnectedRouterProps = {
   children?: React.ReactNode;
   reducerKey?: string;
   Router?: SingletonRouter;
-  context?: React.Context<ReactReduxContextValue>;
   exportTrailingSlash?: boolean;
 }
 
@@ -23,16 +22,12 @@ const createConnectedRouter = (structure: Structure): React.FC<ConnectedRouterPr
    * to update router state in redux store.
    */
   const ConnectedRouter: React.FC<ConnectedRouterProps> = props => {
-    const Context = props.context || ReactReduxContext
-
-    if (Context == null) {
-      throw 'connected-react-router@^1.0.0 requires react-redux v6. ' +
-        'If you are using react-redux v5, install connected-react-router@^0.0.1.'
-    }
-
-    const { store } = useContext(Context)
     const Router = props.Router || NextRouter
     const { exportTrailingSlash = false, reducerKey = 'router' } = props
+    const dispatch = useDispatch()
+    const storeLocation = useSelector((state) => {
+      return getIn(state, [reducerKey, 'location']) as LocationState
+    })
     const isTimeTravelEnabled = useRef(false)
     const inTimeTravelling = useRef(false)
 
@@ -45,44 +40,37 @@ const createConnectedRouter = (structure: Structure): React.FC<ConnectedRouterPr
     }
 
     useEffect(() => {
-      function listenStoreChanges(): void {
-        /**
-         * Next.js asynchronously loads routes, and Redux actions can be
-         * dispatched during this process before Router's history change.
-         * To prevent time travel changes during it, time travel detection
-         * is disabled when Router change starts, and later enabled on change
-         * completion or error.
-         */
-        if (!isTimeTravelEnabled.current) {
-          return
-        }
-        // Extract store's location
-        const storeLocation = getIn(store.getState(), [reducerKey, 'location'])
-        const {
-          pathname: pathnameInStore,
-          search: searchInStore,
-          hash: hashInStore,
-          href
-        } = storeLocation as LocationState
-        // Extract Router's location
-        const historyLocation = locationFromUrl(Router.asPath)
-        const { pathname: pathnameInHistory, search: searchInHistory, hash: hashInHistory } = historyLocation
-        // If we do time travelling, the location in store is changed but location in Router is not changed
-        const locationMismatch =
-          pathnameInHistory !== pathnameInStore || searchInHistory !== searchInStore || hashInStore !== hashInHistory
-        if (locationMismatch) {
-          const as = `${pathnameInStore}${searchInStore}${hashInStore}`
-          // Update Router's location to match store's location
-          if (!inTimeTravelling.current) {
-            inTimeTravelling.current = true
-            Router.replace(href, as)
-          }
+      /**
+       * Next.js asynchronously loads routes, and Redux actions can be
+       * dispatched during this process before Router's history change.
+       * To prevent time travel changes during it, time travel detection
+       * is disabled when Router change starts, and later enabled on change
+       * completion or error.
+       */
+      if (!isTimeTravelEnabled.current) {
+        return
+      }
+      const {
+        pathname: pathnameInStore,
+        search: searchInStore,
+        hash: hashInStore,
+        href
+      } = storeLocation
+      // Extract Router's location
+      const historyLocation = locationFromUrl(Router.asPath)
+      const { pathname: pathnameInHistory, search: searchInHistory, hash: hashInHistory } = historyLocation
+      // If we do time travelling, the location in store is changed but location in Router is not changed
+      const locationMismatch =
+        pathnameInHistory !== pathnameInStore || searchInHistory !== searchInStore || hashInStore !== hashInHistory
+      if (locationMismatch) {
+        const as = `${pathnameInStore}${searchInStore}${hashInStore}`
+        // Update Router's location to match store's location
+        if (!inTimeTravelling.current) {
+          inTimeTravelling.current = true
+          Router.replace(href, as)
         }
       }
-
-      const unsubscribeStore = store.subscribe(listenStoreChanges)
-      return unsubscribeStore
-    }, [Router, reducerKey, store])
+    }, [Router, storeLocation])
 
     useEffect(() => {
       let unpatchRouter = (): void => {}
@@ -90,7 +78,7 @@ const createConnectedRouter = (structure: Structure): React.FC<ConnectedRouterPr
       function listenRouteChanges(url: string, as: string, action: RouterAction): void {
         // Dispatch onLocationChanged except when we're time travelling
         if (!inTimeTravelling.current) {
-          store.dispatch(onLocationChanged(locationFromUrl(url, as), action))
+          dispatch(onLocationChanged(locationFromUrl(url, as), action))
         } else {
           inTimeTravelling.current = false
         }
@@ -111,7 +99,7 @@ const createConnectedRouter = (structure: Structure): React.FC<ConnectedRouterPr
         Router.events.off('routeChangeComplete', enableTimeTravel)
         Router.events.off('connectedRouteChangeComplete', listenRouteChanges)
       }
-    }, [Router, reducerKey, store, exportTrailingSlash])
+    }, [Router, reducerKey, dispatch, exportTrailingSlash])
 
     return <>{props.children}</>
   }
