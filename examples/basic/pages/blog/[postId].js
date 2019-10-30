f8a0b9f4d1be2c808b66425a5d6932f1387a36ed
Daniel Reinoso <danielreinoso1807@gmail.com>
2019-10-30 18:34:33 -0300

    Update react-redux to 7.1.0 (with hooks) (#28)
    * Update react-redux to 7.1.0 (with hooks)
    
    * Update example to use hooks

--- examples/basic/pages/blog/[postId].js
+++ examples/basic/pages/blog/[postId].js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navigation from '../../components/navigation'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-const Blog = ({ routerState }) => (
-  <div>
-    <h1>Blog</h1>
-    <pre>{JSON.stringify(routerState)}</pre>
-    <Navigation />
-  </div>
-)
+const Blog = () => {
+  const routerState = useSelector(state => state.router)
+  return (
+    <div>
+      <h1>Blog</h1>
+      <pre>{JSON.stringify(routerState)}</pre>
+      <Navigation />
+    </div>
+  )
+}
 
-export default connect(state => ({ routerState: state.router }))(Blog)
+export default Blog
--- examples/typescript/pages/about.tsx
+++ examples/typescript/pages/about.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import Navigation from '../components/navigation'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { State } from '../typings'
-import { RouterState } from 'connected-next-router/types'
 
-type AboutProps = {
-  routerState: RouterState;
-}
-
-const About = ({ routerState }: AboutProps) => (
+const About = () => {
+  const routerState = useSelector((state: State) => state.router)
+  return (
   <div>
     <h1>About</h1>
     <pre>{JSON.stringify(routerState)}</pre>
     <Navigation />
   </div>
-)
+)}
 
-export default connect((state: State) => ({ routerState: state.router }))(About)
+export default About
