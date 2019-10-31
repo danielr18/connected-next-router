220db23db6f304368522f3b0a660ff9cde0a9337
Daniel Reinoso <danielreinoso1807@gmail.com>
2019-10-30 21:00:09 -0300

    Update examples

--- examples/basic/pages/_app.js
+++ examples/basic/pages/_app.js
@@ -1,4 +1,4 @@
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import React from 'react'
 import { Provider } from 'react-redux'
 import withRedux from 'next-redux-wrapper'
@@ -9,13 +9,11 @@ class ExampleApp extends App {
   render() {
     const { Component, pageProps, store } = this.props
     return (
-      <Container>
-        <Provider store={store}>
-          <ConnectedRouter>
-            <Component {...pageProps} />
-          </ConnectedRouter>
-        </Provider>
-      </Container>
+      <Provider store={store}>
+        <ConnectedRouter>
+          <Component {...pageProps} />
+        </ConnectedRouter>
+      </Provider>
     )
   }
 }
--- examples/typescript/pages/_app.tsx
+++ examples/typescript/pages/_app.tsx
@@ -1,4 +1,4 @@
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import React from 'react'
 import { Provider } from 'react-redux'
 import withRedux from 'next-redux-wrapper'
@@ -20,13 +20,11 @@ class ExampleApp extends App<AppProps> {
   render() {
     const { Component, pageProps, store } = this.props
     return (
-      <Container>
-        <Provider store={store}>
-          <ConnectedRouter exportTrailingSlash={process.env.__NEXT_EXPORT_TRAILING_SLASH}>
-            <Component {...pageProps} />
-          </ConnectedRouter>
-        </Provider>
-      </Container>
+      <Provider store={store}>
+        <ConnectedRouter exportTrailingSlash={process.env.__NEXT_EXPORT_TRAILING_SLASH}>
+          <Component {...pageProps} />
+        </ConnectedRouter>
+      </Provider>
     )
   }
 }
